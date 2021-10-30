51400ea771b6468de98376d73ca1279c3b014f4e
ygautomo <y_gautomo@yahoo.com>
2021-10-30 18:06:31 +0000

    create project

---
+++ 0305-index.js
@@ -0,0 +1,15 @@
+/**
+ * Filename: 0402-webservernative.js
+ * Course: Dicoding- Belajar Membuat Aplikasi Back-End untuk Pemula
+ * Description: Module 0402 Membangun Web Service menggunakan Node.js- Membangun Web Server secara Native
+ * Repository: 5501-dicoding-backend-basic
+ * 
+ * Author: Yugo Gautomo
+ * Status: Final August 01, 2021
+ */
+
+const message = (name) => {
+   console.log(`Hello ${name}`);
+}
+ 
+message('JavaScript');
